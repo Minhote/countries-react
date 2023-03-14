90dc51ef44111951d1e71e2e1e4ae385a304455b
Erick <erick_icaza_94@hotmail.com>
2023-03-14 10:32:02 -0500

    Borders Functionality Done

--- src/components/Border.jsx
+++ src/components/Border.jsx
@@ -1,3 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 export const Border = ({ text }) => {
-  return <span className="text-light-white dark:text-dark-white bg-light-main dark:bg-dark-main p-2 m-2">{text}</span>;
+  const navigate = useNavigate();
+
+  return (
+    <span
+      className="text-base text-dark-main dark:text-dark-white bg-light-main dark:bg-dark-main p-2 m-2 rounded cursor-pointer hover:bg-dark-main hover:text-light-white hover:dark:bg-light-gray hover:dark:text-dark-main"
+      onClick={() => {
+        if (text.includes(" ")) {
+          navigate(`/details/${text.replaceAll(" ", "_").toLowerCase()}`, {
+            replace: true,
+          });
+        } else {
+          navigate(`/details/${text.toLowerCase()}`, { replace: true });
+        }
+      }}
+    >
+      {text}
+    </span>
+  );
 };
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -10,32 +10,32 @@ export const CountryCard = ({
 }) => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col gap-1 bg-light-main dark:bg-dark-main p-3 rounded-md border-4 border-light-gray dark:border-dark-gray">
+    <div className="flex flex-col gap-5 bg-light-main dark:bg-dark-main p-3 rounded-md border-4 border-light-gray dark:border-dark-gray">
       <figure className="h-52 min-h-[13rem] flex items-center justify-center">
         <img className="h-3/4 w-full text-light-white" src={url} alt={alt} />
       </figure>
       <div className="flex flex-col items-start justify-center gap-2 h-1/2">
-        <h2 className="text-bold text-xl text-light-white dark:text-dark-white">
+        <h2 className="font-bold text-xl text-dark-main dark:text-dark-white">
           {name}
         </h2>
-        <p className="text-bold text-sm text-light-white dark:text-dark-white">
+        <p className="text-bold text-sm text-dark-main dark:text-dark-white">
           <strong className="text-lg">Population:</strong>
           {"  "}
           {Number(population).toLocaleString("en-US")}
         </p>
-        <p className="text-bold text-sm text-light-white dark:text-dark-white">
+        <p className="text-bold text-sm text-dark-main dark:text-dark-white">
           <strong className="text-lg">Region:</strong>
           {"  "}
           {region}
         </p>
-        <p className="text-bold text-sm text-light-white dark:text-dark-white">
+        <p className="text-bold text-sm text-dark-main dark:text-dark-white">
           <strong className="text-lg">Capital:</strong>
           {"  "}
           {capital}
         </p>
       </div>
       <button
-        className="text-base font-medium text-light-gray dark:text-dark-gray underline hover:text-light-secondary"
+        className="text-base font-medium text-dark-main dark:text-dark-gray underline hover:font-bold"
         onClick={() => {
           if (name.includes(" ")) {
             navigate(`/details/${name.replaceAll(" ", "_").toLowerCase()}`);
